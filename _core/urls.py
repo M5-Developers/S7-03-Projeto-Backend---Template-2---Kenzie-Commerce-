9c1db165ef040135093ebdd11711ab8b16a5ee15
luis-mendes-95 <luis.mendes.95@hotmail.com>
2023-05-02 10:07:06 -0300

    create first structure

--- _core/urls.py
+++ _core/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for _commerce project.
+URL configuration for _core project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,8 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("api/", include("accounts.urls")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/docs/swagger/", SpectacularSwaggerView.as_view(url_name="schema")
+    ),
+    path("api/docs/redoc/", SpectacularRedocView.as_view(url_name="schema")),
 ]
