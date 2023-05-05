4357f773a5234ffba998386ef206edf0eb0cd56e
Gabriel Pavão <gabpavao@outlook.com>
2023-05-05 10:28:00 -0300

    feat: create RetrieveAPIView in cart route

--- carts/urls.py
+++ carts/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("accounts/<int:account_id>/cart/", views.CartView.as_view()),
+    path("accounts/cart/", views.CartView.as_view()),
 ]
--- carts/views.py
+++ carts/views.py
@@ -1,4 +1,18 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from . import models, serializers
+import ipdb
 
-class CartView(generics.ListCreateAPIView):
-    ...
+class CartView(generics.RetrieveAPIView):
+	authentication_classes = [JWTAuthentication]
+	permission_classes = [permissions.IsAuthenticated]
+	
+	queryset = models.Cart.objects.all()
+	serializer_class = serializers.CartSerializer
+	pagination_class = None
+
+	def get_object(self):
+		account_id = self.request.user.id
+		cart = self.queryset.get(account_id=account_id)
+		ipdb.set_trace()
+		return cart
