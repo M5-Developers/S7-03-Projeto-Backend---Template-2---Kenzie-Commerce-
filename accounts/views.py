5b9f33060d4ae6a78cadc3880898c348e519dac5
eliasjp <illumine1998@gmail.com>
2023-05-06 09:01:00 -0300

    create for documentation accounts

--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,8 @@ from .models import Account
 from rest_framework import generics
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
-from drf_spectacular.utils import extend_schema
+from drf_spectacular.utils import extend_schema, OpenApiParameter
+from drf_spectacular.types import OpenApiTypes
 
 
 # MRO - Method Resolution Order
@@ -15,11 +16,22 @@ class AccountView(generics.ListCreateAPIView):
     @extend_schema(
         operation_id="accounts_list",
         responses={200: AccountSerializer},
-        description="Rota de listagem de contas de usuários",
-        summary="Sumário Listagem de Contas de usuário",
-        tags=["Tag Listagem de Contas de usuários"],
+        description="Rota de listagem de contas de usuários, qualquer usuário possui acesso a essa informação",
+        summary="Listagem de todos os usuários",
+        tags=["Rotas de Accounts"],
     )
     def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+    
+    @extend_schema(
+        operation_id="accounts_create",
+        responses={201: AccountSerializer},
+        parameters=[AccountSerializer],
+        description="Rota de criação de contas de usuários, ",
+        summary="Criação de usuário",
+        tags=["Rotas de Accounts"],
+    )
+    def post(self, request, *args, **kwargs):
         return super().get(request, *args, **kwargs)
 
 
@@ -31,3 +43,55 @@ class AccountDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = AccountSerializer
 
     lookup_url_kwarg = "account_id"
+
+    @extend_schema(
+        operation_id="accounts_list_by_id",
+        responses={200: AccountSerializer},
+        description="Rota de listagem de um usuário, apenas o proprio usuário pode acessar essa rota",
+        summary="Lista um usuário especificado pelo ID",
+        tags=["Rotas de Accounts"],
+    )
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+    
+    @extend_schema(
+            operation_id="accounts_put",
+            responses={200: AccountSerializer},
+            parameters=[
+                AccountSerializer,
+                OpenApiParameter("account_id", OpenApiTypes.UUID, OpenApiParameter.PATH)
+            ],
+            summary="Altera totalmente as informações do usuário, apenas o proprio usuário pode fazer alteração na sua conta",
+            description="Rota permite fazer uma alteração em um usuário através do ID passado pelo parâmetro 'account_id'",
+            tags=["Rotas de Accounts"]
+    )
+    def put(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+    
+    @extend_schema(
+            operation_id="accounts_patch",
+            responses={200: AccountSerializer},
+            parameters=[
+                AccountSerializer,
+                OpenApiParameter("account_id", OpenApiTypes.UUID, OpenApiParameter.PATH)
+            ],
+            summary="Altera parcialmente informações do usuário, apenas o proprio usuário pode fazer alteração na sua conta",
+            description="Rota permite fazer uma alteração em um usuário através do ID passado pelo parâmetro 'account_id'",
+            tags=["Rotas de Accounts"]
+    )
+    def patch(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+    
+    @extend_schema(
+            operation_id="accounts_delete",
+            responses={200: AccountSerializer},
+            parameters=[
+                AccountSerializer,
+                OpenApiParameter("account_id", OpenApiTypes.UUID, OpenApiParameter.PATH)
+            ],
+            summary="Deleta um usuário, apenas o proprio usuário pode fazer alteração na sua conta",
+            description="Rota permite deletar um usuário através do ID passado pelo parâmetro 'account_id'",
+            tags=["Rotas de Accounts"]
+    )
+    def delete(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
