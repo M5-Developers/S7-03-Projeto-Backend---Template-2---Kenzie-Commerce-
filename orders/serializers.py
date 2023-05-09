2883b6756de75fc2f99f8bf322aecbebcd564d07
Gabriel Pavão <gabpavao@outlook.com>
2023-05-09 09:40:14 -0300

    feat: create product order and list orders

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,19 +1,31 @@
 from rest_framework import serializers
-from .models import Order
+from .models import Order, ProductOrder
 
 class OrderSerializer(serializers.ModelSerializer):
-
     class Meta:
         model=Order
         fields=[
             'id',
             'user_id',
             'status',
-            'created_at'
+            'created_at',
+            'products'
         ]
         depth=1
         extra_kwargs={
             'id':{'read_only':True},
             'created_at':{'read_only':True},
             'user_id':{'read_only':True}
+        }
+
+class ProductOrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductOrder
+        fields = [
+            'order_id',
+            'product'
+            'quantity',
+		]
+        extra_kwargs={
+            'quantity':{'read_only': True},
         }
--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
 from rest_framework import generics
-from .serializers import OrderSerializer,Order
+from .serializers import OrderSerializer
+from .models import Order, ProductOrder
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
 from utils.sendEmail import send_html
@@ -8,24 +8,30 @@ from accounts.models import Account
 from accounts.permissions import IsSeller
 
 class OrderView(generics.ListCreateAPIView):
+	authentication_classes = [JWTAuthentication]
+	permission_classes = [IsAuthenticated]
 
-    authentication_classes=[JWTAuthentication]
-    permission_classes=[IsAuthenticated]
+	queryset = Order.objects.all()
+	serializer_class = OrderSerializer
 
-    serializer_class=OrderSerializer
-    queryset=Order.objects.all()
+	def perform_create(self, serializer):
+		send_html(self.request.user.email,'Pending')
+		user = self.request.user
+		serializer.save(user=user)
 
-    def perform_create(self, serializer):
-        send_html(self.request.user.email,'Pending')
-        serializer.save(user=self.request.user)
+		order_id = serializer.data['id']
+		products = user.cart.products.get_queryset()
+		
+		for product in products:
+			ProductOrder.objects.create(order_id=order_id, product=product, quantity=user.cart.cart_products.select_related().get(product_id=product.id).quantity)
+
 
 class OrderViewDetail(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsSeller]
 
-    authentication_classes=[JWTAuthentication]
-    permission_classes=[IsSeller]
-
-    serializer_class=OrderSerializer
-    queryset=Order.objects.all()
+    serializer_class = OrderSerializer
+    queryset = Order.objects.all()
 
     def perform_update(self, serializer):
         order_pk=self.kwargs['pk']
@@ -34,3 +40,15 @@ class OrderViewDetail(generics.RetrieveUpdateDestroyAPIView):
         send_html(user_email,self.request.data['status'])
         
         serializer.save()
+		
+class OrderInAccountView(generics.ListAPIView):
+	authentication_classes = [JWTAuthentication]
+	permission_classes = [IsAuthenticated]
+
+	queryset = Order.objects.all()
+	serializer_class = OrderSerializer
+
+	def get_queryset(self):
+		orders = self.queryset.filter(user_id=self.request.user.id)
+		
+		return orders
